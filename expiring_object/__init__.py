0abc2ae279c0695a03b63047b8d643defa4d984a
sakost <sakost01+git@gmail.com>
2018-04-25 19:30:20 +0300

    Added little fixes for uploading project to a PyPI

---
+++ expiring_object/__init__.py
@@ -0,0 +1,6 @@
+from .expiring_object import object_factory, Dispatcher
+
+__all__ = [
+    'Dispatcher',
+    'object_factory'
+]
--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 import os
 
-from distutils.core import setup
+from setuptools import setup
 
 with open(os.path.join(os.curdir, 'README.md'), 'r') as file:
     long = file.read()
 
 setup(
     name='expiring_object',
-    version='0.0.1a1',
+    version='0.0.1a3',
     packages=['expiring_object'],
     url='https://github.com/sakost/expiring_object',
     license='MIT',
     author='sakost',
+    author_email='sakost01+expiring_object@gmail.com',
     description='Provides making self-removal objects',
     long_description=long,
     classifiers=[
@@ -26,5 +27,9 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
+    ],
+    keywords='expiring object self-removal',
+    include_package_data=False,
+    test_suite='tests',
+    long_description_content_type='text/markdown'
 )
